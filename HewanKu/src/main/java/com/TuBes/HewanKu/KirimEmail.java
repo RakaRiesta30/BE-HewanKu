01275ad637483d49c1bcbe0200ca9ce9ccdcfa38
RakaRiesta30 <rakariesta30@gmail.com>
2025-12-11 19:14:30 +0700

    error dikit

--- HewanKu/src/main/java/com/TuBes/HewanKu/KirimEmail.java
+++ HewanKu/src/main/java/com/TuBes/HewanKu/KirimEmail.java
@@ -1,11 +1,18 @@
 package com.TuBes.HewanKu;
 
-import java.util.*;
+import java.util.Properties;
+import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.*;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class KirimEmail {
