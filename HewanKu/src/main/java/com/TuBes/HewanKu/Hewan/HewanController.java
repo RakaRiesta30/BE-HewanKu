1a50769f341bd51613d2b12e4107e46ee1b0442f
RakaRiesta30 <rakariesta30@gmail.com>
2025-12-29 01:48:15 +0700

    ngerapiin

--- HewanKu/src/main/java/com/TuBes/HewanKu/Hewan/HewanController.java
+++ HewanKu/src/main/java/com/TuBes/HewanKu/Hewan/HewanController.java
@@ -3,13 +3,10 @@ package com.TuBes.HewanKu.Hewan;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,33 +16,18 @@ public class HewanController {
     @Autowired
     private HewanService hewanService;
 
-    @PostMapping("/add")
-    public Map<String, Object> createHewan(@RequestBody HewanDTO hewanDTO,@PathVariable Long id){
-        return hewanService.createHewan(hewanDTO, id);
-    }
-
     @GetMapping("")
-    public Map<String, Object> viewHewan(@RequestParam String role ,@PathVariable Long id){
-        return hewanService.viewHewan(role, id);
-    }
-
-    @PostMapping("/edit/{idHewan}")
-    public Map<String, Object> editHewan(@RequestBody HewanDTO hewanDTO, @PathVariable Long id, @PathVariable Long idHewan){
-        return hewanService.editHewan(hewanDTO, id, idHewan);
-    }
-
-    @DeleteMapping("/delete")
-    public Map<String, Object> deleteHewan(@PathVariable Long id, @RequestParam Long idHewan){
-        return hewanService.deleteHewan(id, idHewan);
+    public Map<String, Object> viewHewan(@PathVariable Long id) {
+        return hewanService.viewHewan(id);
     }
 
     @GetMapping("/filter")
-    public Map<String, Object> filterHewan(@PathVariable Long id,@RequestBody FilterDTO filterDTO){
+    public Map<String, Object> filterHewan(@PathVariable Long id, @RequestBody FilterDTO filterDTO) {
         return hewanService.filterHewan(id, filterDTO);
     }
 
     @GetMapping("/{idHewan}")
-    public Map<String, Object> detailHewan(@PathVariable Long id, @PathVariable Long idHewan){
+    public Map<String, Object> detailHewan(@PathVariable Long id, @PathVariable Long idHewan) {
         return hewanService.detailHewan(id, idHewan);
     }
 }
--- HewanKu/src/main/java/com/TuBes/HewanKu/Shelter/ShelterController.java
+++ HewanKu/src/main/java/com/TuBes/HewanKu/Shelter/ShelterController.java
@@ -3,18 +3,24 @@ package com.TuBes.HewanKu.Shelter;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.TuBes.HewanKu.Hewan.HewanDTO;
+import com.TuBes.HewanKu.Hewan.HewanService;
 
 @RestController
 @RequestMapping("/shelter")
 public class ShelterController {
     @Autowired
     private ShelterService shelterService;
+    private HewanService hewanService;
 
     @PostMapping("/register")
     public Map<String, Object> register(@RequestBody ShelterDTO shelterDTO) {
@@ -60,5 +66,21 @@ public class ShelterController {
     @GetMapping("/view/{idShelter}")
     public Map<String, Object> viewShelter(@PathVariable Long idShelter){
         return shelterService.viewShelter(idShelter);
+    }
+
+    @DeleteMapping("/delete")
+    public Map<String, Object> deleteHewan(@PathVariable Long id, @RequestParam Long idHewan) {
+        return hewanService.deleteHewan(id, idHewan);
+    }
+
+    @PostMapping("/add")
+    public Map<String, Object> createHewan(@RequestBody HewanDTO hewanDTO, @PathVariable Long id) {
+        return hewanService.createHewan(hewanDTO, id);
+    }
+
+    @PostMapping("/edit/{idHewan}")
+    public Map<String, Object> editHewan(@RequestBody HewanDTO hewanDTO, @PathVariable Long id,
+            @PathVariable Long idHewan) {
+        return hewanService.editHewan(hewanDTO, id, idHewan);
     }
 }
